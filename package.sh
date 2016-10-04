425daad0c2e2713f14ff738566f42ac1c5a2b1ce
Lee Archer <lee@connected-ventures.com>
2016-10-04 08:51:16 +0100

    Remove .gitignore from deb_<service>/usr/local/bin
    We don't need it anymore as we never build locally

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@
 # mkdebdir - mkdir -p $DEB_DIR$1 if it does not exist
 
 mkdebdir /usr/local/bin
-mkdebdir /etc/$NAME
+mkdebdir /etc/f8-$NAME
 mkdebdir /etc/init
 
 cp ./bin/$NAME $DEB_DIR/usr/local/bin/f8-$NAME
